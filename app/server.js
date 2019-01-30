7b55126db2473063fd6cfef6a23ca92a13cb2cb4
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-31 01:04:34 +0300

    add highlight for active page + use bootstrap

--- app/server.js
+++ app/server.js
@@ -22,9 +22,17 @@ app.use(express.static(path.join(__dirname, 'static')));
 // } handlebars
 
 // endpoints {
-app.get('/', (req, res) => res.render('home'));
-app.get('/features', (req, res) => res.render('features'));
-app.get('/news', (req, res) => res.render('news'));
+app.get('/', (req, res) => {
+  res.render('home', { home: true });
+});
+
+app.get('/features', (req, res) => {
+  res.render('features', { features: true });
+});
+
+app.get('/news', (req, res) => {
+  res.render('news', { news: true });
+});
 // } endpoints
 
 // export {
