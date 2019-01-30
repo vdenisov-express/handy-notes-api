baff15dad95986635086d27fca78ebd6eda6f00a
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-30 23:16:51 +0300

    add .hbs templates and configuration for handlebars

--- app/server.js
+++ app/server.js
@@ -1,12 +1,29 @@
 // libs {
-const express = require('express')
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path = require('path');
 // } libs
-
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// handlebars {
+const hbsConfig = exphbs.create({
+  layoutsDir: path.join(__dirname, 'views/layouts/'),
+  defaultLayout: 'main',
+  extname: '.hbs',
+});
 
+app.engine('.hbs', hbsConfig.engine);
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+// } handlebars
+
+// endpoints {
+app.get('/', (req, res) => {
+  res.render('home');
+});
+// } endpoints
 
 // export {
 module.exports = app;
