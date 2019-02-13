7870a7ca24fe98cad1c097ac41bad7d39511ac7f
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-13 14:04:14 +0300

    sweeten the syntax

--- db/migrations/sqls/20190213110007-relationship-Likes-up.sql
+++ db/migrations/sqls/20190213110007-relationship-Likes-up.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS "Likes" (
 
   UNIQUE("Notes_id", "Users_id"),
 
-  CONSTRAINT Likes_fk_Notes_id
+  CONSTRAINT "Likes_fk_Notes_id"
     FOREIGN KEY ("Notes_id") REFERENCES "Notes" ("id")
       ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT Likes_fk_Users_id
+  CONSTRAINT "Likes_fk_Users_id"
     FOREIGN KEY ("Users_id") REFERENCES "Users" ("id")
       ON UPDATE CASCADE ON DELETE CASCADE
 );
