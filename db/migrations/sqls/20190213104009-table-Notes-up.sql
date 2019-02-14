7ae148aab6f78f9107a504d5ba46ddc6e37cbf72
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-14 13:38:52 +0300

    One user can't have two or more notes with same titles

--- db/migrations/sqls/20190213104009-table-Notes-up.sql
+++ db/migrations/sqls/20190213104009-table-Notes-up.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS "Notes" (
   "text"      TEXT      NOT NULL,
   "Users_id"  INTEGER   NOT NULL,
 
+  UNIQUE("title", "Users_id"),
+
   CONSTRAINT "Notes_fk_Users_id"
     FOREIGN KEY ("Users_id") REFERENCES "Users" ("id")
       ON UPDATE CASCADE ON DELETE CASCADE
