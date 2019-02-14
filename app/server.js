3abd2fc74c2c77e2f168d367b39182479f1d0eed
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-14 13:15:56 +0300

    add endpoint for unknown requests

--- app/server.js
+++ app/server.js
@@ -46,6 +46,10 @@ app.get('/news', (req, res) => {
 });
 // } endpoints
 
+app.use('**', (req, res) => {
+  res.status(404).send('404 - Not Found :(');
+});
+
 // export {
 module.exports = app;
 // } export
