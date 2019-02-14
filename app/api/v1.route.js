d7f61a605ab0d3fdb75c208f55b88f970a5aed2e
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-14 13:32:01 +0300

    add notes' routes to api v1

--- app/api/v1.route.js
+++ app/api/v1.route.js
@@ -1,6 +1,7 @@
 const apiV1 = require('express').Router();
 
 const { usersRoute } = require('./users/users.route');
+const { notesRoute } = require('./notes/notes.route');
 
 
 apiV1.get('/', (req, res) => {
@@ -8,6 +9,7 @@ apiV1.get('/', (req, res) => {
 });
 
 apiV1.use('/users', usersRoute);
+apiV1.use('/notes', notesRoute);
 
 
 module.exports = { apiV1 };
