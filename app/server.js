b28c772a342c86f89a12ddc0cc76cefbac946b86
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-18 15:22:52 +0300

    move all files for "site" in separate module

--- app/server.js
+++ app/server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const path = require('path');
-const exphbs  = require('express-handlebars');
+const initSiteFor = require('./site/init');
 // } libs
 
 const { apiV1 } = require('./api/v1.route');
@@ -13,20 +12,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./api/swagger.json');
 
-// handlebars {
-const hbsConfig = exphbs.create({
-  layoutsDir: path.join(__dirname, 'views/layouts/'),
-  defaultLayout: 'main',
-  extname: '.hbs',
-});
-
-app.engine('.hbs', hbsConfig.engine);
-
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'static')));
-// } handlebars
+initSiteFor(app);
 
 // API v1 {
 app.use(bodyParser.json());
@@ -35,20 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true }) );
 app.use('/api-v1', apiV1);
 app.use('/api-v1-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // } API v1
-
-// endpoints {
-app.get('/home', (req, res) => {
-  res.render('home', { home: true });
-});
-
-app.get('/features', (req, res) => {
-  res.render('features', { features: true });
-});
-
-app.get('/news', (req, res) => {
-  res.render('news', { news: true });
-});
-// } endpoints
 
 app.use('**', (req, res) => {
   res.status(404).send('404 - Not Found :(');
