f363824571b0846cf70b59d83eee0e30c11992a1
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-19 18:07:10 +0300

    add tests for server endpoints

---
+++ test/init.js
@@ -0,0 +1,73 @@
+const chai = require('chai');
+const supertest = require('supertest');
+
+
+const request = supertest('http://localhost:3000');
+
+
+describe('>>> SERVER <<<', () => {
+
+  it('GET / should return status 404', (done) => {
+    request
+      .get('/')
+      .end((err, res) => {
+        chai.assert.equal(res.status, 404);
+        chai.assert.typeOf(res.text, 'string');
+        chai.assert.equal(res.text, '404 - Not Found :(');
+        done(err);
+      });
+  });
+
+  it('GET /api/v1 should return status 200', (done) => {
+    request
+      .get('/api/v1')
+      .end((err, res) => {
+        chai.assert.equal(res.status, 200);
+        chai.assert.typeOf(res.text, 'string');
+        chai.assert.equal(res.text, 'Welcome to API v1 !!!');
+        done(err);
+      });
+  });
+
+  it('GET /abcdefg should return status 404', (done) => {
+    request
+      .get('/abcdefghijk')
+      .end((err, res) => {
+        chai.assert.equal(res.status, 404);
+        chai.assert.typeOf(res.text, 'string');
+        chai.assert.equal(res.text, '404 - Not Found :(');
+        done(err);
+      });
+  });
+
+});
+
+
+
+describe('>>> API v1 <<<', () => {
+
+  describe.skip(`
+  #########
+  # USERS #
+  #########
+  `, () => {
+    require('./users.test');
+  });
+
+  describe.skip(`
+  #########
+  # NOTES #
+  #########
+  `, () => {
+    require('./notes.test');
+  });
+
+  describe.skip(`
+  ########
+  # TAGS #
+  ########
+  `, () => {
+    require('./tags.test');
+  });
+
+});
