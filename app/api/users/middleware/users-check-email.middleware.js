ac4a57874aa1d5d68456da76c673b7ae6e5c9b98
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-25 18:27:43 +0300

    fix middlewares for checking "name" and "email"

--- app/api/users/middleware/users-check-email.middleware.js
+++ app/api/users/middleware/users-check-email.middleware.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     .checkEmail(req.body.email)
 
     .then(userObj => {
-      userObj ? next() : handlerFor.ERROR_NOT_FOUND(res, 'this `email` is already in use')
+      !userObj ? next() : handlerFor.ERROR_NOT_FOUND(res, 'this `email` is already in use')
     })
 
     .catch(err => handlerFor.ERROR_ON_DATABASE(res, err));
--- app/api/users/middleware/users-check-name.middleware.js
+++ app/api/users/middleware/users-check-name.middleware.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     .checkName(req.body.name)
 
     .then(userObj => {
-      userObj ? next() : handlerFor.ERROR_NOT_FOUND(res, 'this `name` is already in use')
+      !userObj ? next() : handlerFor.ERROR_NOT_FOUND(res, 'this `name` is already in use')
     })
 
     .catch(err => handlerFor.ERROR_ON_DATABASE(res, err));
