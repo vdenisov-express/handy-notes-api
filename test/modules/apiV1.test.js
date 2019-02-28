5fc1b06d188e8ffb6d3397191591a88fe2070cf6
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-28 14:54:42 +0300

    auth tests in priority

--- test/modules/apiV1.test.js
+++ test/modules/apiV1.test.js
@@ -1,9 +1,7 @@
+describe(`\n### AUTH ###\n`,  () => { require('./../../app/api/auth/test/auth.test');   });
+
 describe(`\n### USERS ###\n`, () => { require('./../../app/api/users/test/users.test'); });
 
 describe(`\n### NOTES ###\n`, () => { require('./../../app/api/notes/test/notes.test'); });
 
 describe(`\n### TAGS ###\n`,  () => { require('./../../app/api/tags/test/tags.test');   });
-
-//////////////////////////////
-
-describe(`\n### AUTH ###\n`,  () => { require('./../../app/api/auth/test/auth.test');   });
