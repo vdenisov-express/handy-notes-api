a1188260efab911b5db11039c07134bb6f23a2a3
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-28 15:46:08 +0300

    add package "helmet"

--- app/server.js
+++ app/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const initSite = require('./site/site.init');
 const initAPI  = require('./api/api.init');
 
+// security {
+const helmet = require('helmet');
+// } security
+
 
 const app = express();
 
@@ -13,5 +17,11 @@ console.log('* app => static site initialized');
 // connecting the API to the application
 initAPI(app);
 console.log('* app => server API initialized');
+
+// security {
+app.use(helmet());
+app.disable('x-powered-by');
+// } security
+
 
 module.exports = app;
