bfcebdbc477ba6f713a9d0e4ad30433fc92ab50d
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-03-28 14:28:32 +0300

    tags - upgrade#1

--- test/modules/api.test.js
+++ test/modules/api.test.js
@@ -4,6 +4,6 @@ describe(`\n### AUTH ###\n`, () => { require('./../../app/api/auth/auth.test'); 
 
 describe(`\n### USERS ###\n`, () => { require('./../../app/api/users/users.test'); });
 
-describe.only(`\n### NOTES ###\n`, () => { require('./../../app/api/notes/notes.test'); });
+describe(`\n### NOTES ###\n`, () => { require('./../../app/api/notes/notes.test'); });
 
-describe(`\n### TAGS ###\n`, () => { require('./../../app/api/tags/test/tags.test'); });
+describe.only(`\n### TAGS ###\n`, () => { require('./../../app/api/tags/tags.test'); });
